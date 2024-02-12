a5dae095ac42b02450e607a9d879c9938dab26d0
Taraldinn <kferdoush617@gmail.com>
2024-02-12 22:04:32 +0600

    done everything remains testing !

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework.authtoken.views import obtain_auth_token
 from django.urls import path
 
-from user_app.api.views import registration_view
+from user_app.api.views import registration_view, login_view
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
-    path('registration/', registration_view, name='registration')
+    path('signup/', registration_view, name='registration'),
+    path('logout/', login_view, name='logout')
 ]
 
--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,18 +1,36 @@
 from rest_framework import status
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
 from user_app.api.serializers import RegistartionSerializers
+from user_app.models import create_auth_token
 
+
+
+@api_view(['POST'])
+def login_view(request):
+    if request.method =='POST':
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK, data={'message': 'successfully logged out'})
 
 @api_view(['POST'])
 def registration_view(request):
     if request.method == 'POST':
         serializer = RegistartionSerializers(data=request.data)
+
+        data = {}
+
         if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+            account = serializer.save()
+            data['response'] = 'successfully registered new user.'
+            data['username'] = account.username
+            data['email'] = account.email
+            token = Token.objects.get(user=account).key
+            data['token'] = token
         else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            data = serializer.errors
+        return Response(data)
     else:
         return Response(status=status.HTTP_405_METHOD_NOT_ALLOWED)
